91ac6af27531bd4fca9330d4c5049907c4c0f9d3
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-07-18 17:51:09 -0300

    home: add header with avatar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,38 @@
-import { getAllPosts } from 'api/posts'
-import { Layout } from 'components/Layout'
+import Image from 'next/image'
+import me from '../../public/images/me.jpg'
 
-type Props = {
-  posts: any[]
-}
+const AVATAR_SIZE = 48
 
-const IndexPage = ({ posts }: Props) => {
-  console.log(posts)
-
+const IndexPage = () => {
   return (
-    <Layout>
-      <h2>Teste</h2>
-    </Layout>
-  )
-}
+    <>
+      <header className="px-2 py-6 w-full">
+        <div className="flex h-full items-center w-full max-w-4xl m-auto">
+          <Image
+            width={AVATAR_SIZE}
+            height={AVATAR_SIZE}
+            placeholder="blur"
+            src={me}
+            className="rounded-full"
+            alt="Esse na foto sou eu, Wallace."
+          />
 
-export const getStaticProps = async () => {
-  const posts = getAllPosts()
-  return { props: { posts } }
+          <div className="px-4">
+            <h2 className="text-gray-600 font-black text-xl">
+              Wallace Oliveira
+            </h2>
+            <p className="text-gray-600">
+              Olá, atualmente trabalho como desenvolvedor frontend
+            </p>
+          </div>
+        </div>
+      </header>
+
+      {/* @TODO featured post */}
+      {/* @TODO last five posts */}
+      {/* @TODO list all posts */}
+    </>
+  )
 }
 
 export default IndexPage
