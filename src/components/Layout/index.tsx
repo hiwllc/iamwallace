7ec252fd5ed80f2bae491d6241395a267eaa0325
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-07-18 20:14:13 -0300

    components: remove layout

--- src/components/Layout/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-export type Props = {
-  children: ReactNode
-}
-
-export function Layout({ children }: Props) {
-  return <div className="min-h-screen bg-gray-50">{children}</div>
-}
