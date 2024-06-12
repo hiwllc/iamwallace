d5b859d02e2edb483e5fc1c2413f29881337b00a
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2024-06-12 01:24:31 -0300

    feat(widget): add react-query

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -1,4 +1,7 @@
-import { MoveUpRightIcon } from 'lucide-preact'
+import type { ReactNode } from 'react'
+import { MoveUpRightIcon } from 'lucide-react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useMusic } from '../hooks/use-music';
 
 type Props = {
   music: {
@@ -7,24 +10,55 @@ type Props = {
     cover: string;
     name: string;
     url: string;
-  }
+  },
+  children: ReactNode
 }
 
-export function WidgetSpotify({ music }: Props) {
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: 'always',
+    }
+  }
+})
+
+function AppProvider({ children }: Omit<Props, 'music'>) {
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+export function Widget() {
+  const { data, status } = useMusic()
+
+  if (status !== 'success' || !data) {
+    return null
+  }
+
   return (
     <div className="fixed bottom-0 right-0 md:bottom-4 md:right-4 w-full md:max-w-sm border-4 flex border-zinc-900 rounded-ss-lg rounded-se-lg md:rounded-lg overflow-hidden p-2 gap-6 bg-white shadow-solig">
-      <img src={music.cover} alt={music.name} className="rounded-md size-28 h-full aspect-square shadow-lg" />
+      <img src={data.cover} alt={data.name} className="rounded-md size-28 h-full aspect-square shadow-lg" />
 
       <div className="min-w-44 leading-tight flex flex-col gap-3">
         <div>
-          <h3 className="font-semibold text-md font-mono">{music.name}</h3>
+          <h3 className="font-semibold text-md font-mono">{data.name}</h3>
           <p className="text-sm font-medium text-orange-500 font-mono">
-            {music.artists.map(({ name }) => name).join(', ')}
+            {data.artists.map(({ name }) => name).join(', ')}
           </p>
         </div>
 
-        <a href={music.url} className="bg-orange-200 font-mono mt-auto w-fit inline-flex items-center gap-2 py-1 px-3 rounded-md text-xs font-medium hover:bg-orange-300">ouvir no spotify <MoveUpRightIcon class="size-4" /></a>
+        <a href={data.url} className="bg-orange-200 font-mono mt-auto w-fit inline-flex items-center gap-2 py-1 px-3 rounded-md text-xs font-medium hover:bg-orange-300">ouvir no spotify <MoveUpRightIcon className="size-4" /></a>
       </div>
     </div>
+  )
+}
+
+export function WidgetSpotify() {
+  return (
+    <AppProvider>
+      <Widget />
+    </AppProvider>
   )
 }
---
+++ src/hooks/use-music.ts
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+
+type Music = {
+  artists: Array<{ name: string }>;
+  background: string;
+  cover: string;
+  name: string;
+  url: string;
+}
+
+type Data = { success: false } | { success: true, music: Music }
+
+export function useMusic() {
+  const { data, status } = useQuery({
+    queryKey: ['FETCH_SPOFITY'],
+    queryFn: async () => {
+      const result = await fetch(new URL(`/api/spotify`, import.meta.env.DEV ? `http://localhost:4321/` : `https://iamwallace.dev/`))
+
+      if (!result.ok) {
+        console.log(result.status)
+        throw new Error(`Failed to fetch`)
+      }
+
+      const data = await result.json() as Data
+
+      if (!data.success) {
+        console.log(result.status)
+        throw new Error(`Failed to fetch`)
+      }
+
+      return data.music
+    },
+  })
+
+  return {
+    data,
+    status,
+  }
+}
